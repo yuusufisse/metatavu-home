5c62f1ab8986358da7d474dbcdf70c881eabe817
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2023-08-31 10:41:27 +0300

    added cast typing and null check for root container

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,13 @@ import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App/>
-  </StrictMode>
-);
+const container = document.getElementById("root") as HTMLElement;
+
+if (container !== null) {
+  const root = createRoot(container);
+  root.render(
+    <StrictMode>
+      <App/>
+    </StrictMode>
+  );
+}
