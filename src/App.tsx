638e19359559dca375ae7277e8d308da24d4777a
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2023-09-05 10:03:35 +0300

    removed props

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,18 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import DashboardScreen from "./components/screens/dashboard/dashboard-screen";
 import ErrorScreen from "./components/screens/error-screen";
 import AuthenticationProvider from "./components/providers/authentication-provider";
-import { useState } from "react";
-import type { KeycloakProfile, KeycloakTokenParsed } from "keycloak-js";
 import ErrorHandler from "./components/contexts/error-handler";
-
-export interface Auth {
-  token: KeycloakTokenParsed 
-  tokenRaw: string
-  logout: () => void
-}
 
 /**
  * Application component
  *
  */
 function App () {
-  const [auth, setAuth] = useState<Auth>();
-  const [userProfile, setUserProfile] = useState<KeycloakProfile>();
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <DashboardScreen auth={auth} />,
+      element: <DashboardScreen />,
       errorElement: <ErrorScreen />
     }
   ]);
@@ -31,11 +21,7 @@ function App () {
   return (
     <div className="App">
       <ErrorHandler>
-        <AuthenticationProvider
-          auth={auth}
-          setAuth={setAuth}
-          userProfile={userProfile}
-          setUserProfile={setUserProfile}>
+        <AuthenticationProvider>
           <RouterProvider router={router} />
         </AuthenticationProvider>
       </ErrorHandler>
