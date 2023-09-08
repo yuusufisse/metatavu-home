05a5115ed7e3c0025f63f6497e34bcf1f0be6ff1
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2023-09-08 13:52:42 +0300

    backup

--- src/components/screens/dashboard/dashboard-screen.tsx
+++ src/components/screens/dashboard/dashboard-screen.tsx
@@ -2,7 +2,7 @@ import { useAtomValue, useSetAtom } from "jotai";
 import { authAtom, userProfileAtom } from "../../../atoms/auth";
 import { useEffect, useState } from "react";
 import { useApi } from "../../../hooks/use-api";
-import { PersonTotalTime } from "../../../generated/client";
+import { Person, PersonTotalTime } from "../../../generated/client";
 import { errorAtom } from "../../../atoms/error";
 import LoaderWrapper from "../../generics/loader-wrapper";
 import { getHoursAndMinutes } from "../../../utils/time-utils";
@@ -17,6 +17,7 @@ function DashboardScreen () {
   const [isLoading, setIsLoading] = useState(false);
   const [personTotalTime, setPersonTotalTime] = useState<PersonTotalTime>();
   const { personsApi } = useApi();
+  const [fetchedPersona, setFetchedPersona] = useState<Person[] | undefined>(); 
   
 /**
  * Initialize logged in person's time data.
@@ -24,19 +25,20 @@ function DashboardScreen () {
   const getPersons = async () => {
     setIsLoading(true);
     const fetchedPersons = await personsApi.listPersons({ active: true });
-    const loggedInPerson = fetchedPersons.filter(person => person.keycloakId === userProfile?.id);
+    // const loggedInPerson = fetchedPersons.filter(person => person.keycloakId === userProfile?.id);
 
-    if (loggedInPerson.length) {
+    // if (loggedInPerson.length) {
       try {
-          const fetchedPerson = await personsApi.listPersonTotalTime({personId: loggedInPerson[0].id});
+          const fetchedPerson = await personsApi.listPersonTotalTime({personId: fetchedPersons[1].id});
+          setFetchedPersona(fetchedPersons);
           setPersonTotalTime(fetchedPerson[0]);
       } catch (error) {
         setError(`${ "Person fetch has failed." }, ${ error }`);
       }
-    }
-    else {
-      setError("Your account does not have any time bank entries.");
-    }
+    // }
+    // else {
+    //   setError("Your account does not have any time bank entries.");
+    // }
     setIsLoading(false);
   }
 
@@ -49,6 +51,9 @@ function DashboardScreen () {
     <LoaderWrapper loading={isLoading}>
       <div>
         {(personTotalTime) ?`Your balance is ${getHoursAndMinutes(Number(personTotalTime?.balance))}` :null}
+      </div>
+      <div>
+        {fetchedPersona ? fetchedPersona[1].firstName : null}
       </div>
       <button type="button" onClick={() => auth?.logout()}>Log out</button>
     </LoaderWrapper>
