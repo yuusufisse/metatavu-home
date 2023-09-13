98d98963d199df40063f51e43e1f2dc1e7766e64
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2023-09-13 10:51:31 +0300

    renamed

---
+++ src/components/screens/vacation-requests/vacation-requests-table/vacation-requests-table-columns.ts
@@ -0,0 +1,35 @@
+import { GridColDef } from "@mui/x-data-grid";
+
+export const columns: GridColDef[] = [
+  { field: 'id', headerName: 'ID', width: 90 },
+  {
+    field: 'type',
+    headerName: 'Vacation Type',
+    width: 150,
+    editable: false,
+  },
+  {
+    field: 'startDate',
+    headerName: 'Start Date',
+    width: 150,
+    editable: false,
+  },
+  {
+    field: 'endDate',
+    headerName: 'End Date',
+    width: 150,
+    editable: false,
+  },
+  {
+    field: 'days',
+    headerName: 'Day Count',
+    width: 150,
+    editable: false,
+  },
+  {
+    field: 'status',
+    headerName: 'Status',
+    width: 150,
+    editable: false,
+  },
+];
