dc4626a93bcc5ad6d3186ff286e5071f7eaeee23
Joonas Rissanen <joonas.rissanen@metatavu.fi>
2023-09-18 16:21:45 +0300

    suggestions implemented

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+  
+/**
+ * @description NavButtonProps
+ * @date 9/18/2023 - 4:11:10 PM
+ *
+ * @export
+ * @interface NavButtonProps
+ * @typedef {NavButtonProps}
+ */
+export interface NavButtonProps {
+    text: string;
+    selected: boolean;
+    sx_props?: object;
+  }
