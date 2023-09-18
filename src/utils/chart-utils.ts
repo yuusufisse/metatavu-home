c7c5213c61fcf5920765285ad270e09f1b679e9a
petri-nykanen <petri.nykaenen@gmail.com>
2023-09-18 16:18:18 +0300

    Bar chart init

--- src/utils/chart-utils.ts
+++ src/utils/chart-utils.ts
@@ -1,9 +1,30 @@
-import { PersonTotalTime } from "../generated/client";
+import { DailyEntry, PersonTotalTime } from "../generated/client";
 import { getHoursAndMinutes } from "./time-utils";
 
-interface CustomLabel {
+export interface CustomLabel {
   value: number;
   name: string;
+}
+
+/**
+ * Type for work time data
+ */
+export interface WorkTimeData {
+  name: string;
+  expected: number;
+  billableProject: number;
+  nonBillableProject: number;
+  internal: number;
+}
+
+/**
+ * Type for work time data
+ */
+export interface WorkTimeTotalData {
+  name: string;
+  balance: number;
+  logged?: number;
+  expected?: number;
 }
 
 export const workTimeData = (personTotalTime: PersonTotalTime | undefined) => {
@@ -17,3 +38,32 @@ export const workTimeData = (personTotalTime: PersonTotalTime | undefined) => {
 export const renderCustomizedLabel = (props: CustomLabel) => {
   return `${props.name}\n${getHoursAndMinutes(props.value)}`;
 }
+
+export const dateEntriesPreprocess = (dateEntries: DailyEntry[]) => {
+  const workTimeData: WorkTimeData[] = [];
+  const workTimeTotalData: WorkTimeTotalData = {
+    name: "WorkTimeData",
+    balance: 0,
+    logged: 0,
+    expected: 0
+  };
+
+  dateEntries.forEach(
+    entry => {
+      const { date, expected, billableProjectTime, nonBillableProjectTime, internalTime, balance, logged } = entry;
+
+      workTimeData.push({
+        name: new Date(date).toLocaleDateString(),
+        expected: expected,
+        billableProject: billableProjectTime,
+        nonBillableProject: nonBillableProjectTime,
+        internal: internalTime
+      });
+      workTimeTotalData.balance += balance;
+      workTimeTotalData.logged += logged;
+      workTimeTotalData.expected += expected;
+    }
+  );
+
+  return { workTimeData: workTimeData, workTimeTotalData: workTimeTotalData };
+};
