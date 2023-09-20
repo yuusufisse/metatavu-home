d0fbef34677a48340524913e251eb664c317099c
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2023-09-20 10:49:36 +0300

    table update

--- src/components/screens/vacation-requests/vacation-requests-table/table-form.tsx
+++ src/components/screens/vacation-requests/vacation-requests-table/table-form.tsx
@@ -1,13 +1,25 @@
-import { Box, Divider, FormControl, FormLabel, Select, TextField } from "@mui/material";
+import {
+  Box,
+  Button,
+  Divider,
+  FormControl,
+  FormLabel,
+  Grid,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  TextField
+} from "@mui/material";
 import { VacationType } from "../../../../generated/client";
 
 import { ChangeEvent, useEffect, useState } from "react";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 
 const TableForm = () => {
   const [message, setMessage] = useState<string>("");
-  const [startDate, setStartDate] = useState<Date>(new Date());
-  const [endDate, setEndDate] = useState<Date>(new Date());
-  const [type, setType] = useState<VacationType>("VACATION");
+  const [startDate, setStartDate] = useState<Date | null>(new Date());
+  const [endDate, setEndDate] = useState<Date | null>(new Date());
+  const [type, setType] = useState<string>("VACATION");
   const [days, setDays] = useState<number>(0);
 
   useEffect(() => {}, []);
@@ -25,17 +37,43 @@ const TableForm = () => {
             <FormLabel>Vacation Type</FormLabel>
             <Select
               value={type}
-              onChange={(event: ChangeEvent<HTMLInputElement>) => {
+              onChange={(event: SelectChangeEvent<string>) => {
                 setType(event.target.value);
               }}
-            />
+              sx={{ marginBottom: "5px" }}
+            >
+              {(Object.keys(VacationType) as Array<keyof typeof VacationType>).map(
+                (vacationType) => {
+                  return <MenuItem value={vacationType}>{vacationType}</MenuItem>;
+                }
+              )}
+            </Select>
             <FormLabel>Message</FormLabel>
             <TextField
               value={message}
               onChange={(event: ChangeEvent<HTMLInputElement>) => {
                 setMessage(event.target.value);
               }}
+              sx={{ marginBottom: "5px" }}
             />
+            <LocalizationProvider />
+            <FormLabel sx={{ marginBottom: "5px" }}>Duration</FormLabel>
+            <Grid container>
+              <DatePicker
+                label="Start Date"
+                value={startDate}
+                minDate={startDate}
+                onChange={(newValue) => setStartDate(newValue)}
+              />
+              <DatePicker
+                label="End Date"
+                value={endDate}
+                onChange={(newValue) => setEndDate(newValue)}
+              />
+            </Grid>
+            <Button type="submit" variant="contained" sx={{ marginTop: "10px" }}>
+              Submit
+            </Button>
           </FormControl>
         </form>
       </Box>
