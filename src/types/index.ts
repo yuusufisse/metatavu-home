3b8cc62478659c752d42dd4f0990b2d441466ea3
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2023-09-20 16:28:47 +0300

    moved all types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,45 @@
+import { VacationType } from "../generated/client";
+
 /**
  * Type describing available languages
  */
 export type Language = "fi" | "en";
+
+export interface DataGridRow {
+  id: string | undefined;
+  type: VacationType;
+  startDate: string;
+  endDate: string;
+  days: number;
+  status: string;
+}
+
+export interface SkeletonTableColumn {
+  variant: string;
+  height: number | string;
+  width: number | string;
+  margin: number | string;
+}
+
+export interface SkeletonTableRow {
+  variant: string;
+  height: string | number;
+  width: string | number;
+  margin: string;
+}
+
+// export interface VacationData {
+//   startDate: Date | undefined | null;
+//   endDate: Date | undefined | null;
+//   type: VacationType | undefined | null;
+//   message: string | undefined | null;
+//   days: number | undefined | null;
+// }
+
+export interface VacationData {
+  startDate: Date;
+  endDate: Date;
+  type: VacationType;
+  message: string;
+  days: number;
+}
