82496eb22d39a03a38b6eb49e1da78bd5e5669a0
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2023-09-21 12:38:51 +0300

    added object null check util

---
+++ src/utils/null-check-utils.ts
@@ -0,0 +1,14 @@
+/**
+ * Checks whether an object has any null or undefined props
+ *
+ * @param object Object to be null-checked on every of its prop
+ * @returns boolean, indicates if any prop is null within the object
+ */
+export const nullCheckObjectProps = (object: object): boolean => {
+  Object.keys(object).map((prop) => {
+    if (prop !== null || prop !== undefined) {
+      return false;
+    }
+  });
+  return true;
+};
