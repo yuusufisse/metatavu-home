b0e22e6fc0cd6ab949ab4212afa8f119b42ffb03
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2023-09-25 10:17:09 +0300

    refactor code

--- src/components/screens/vacation-requests/vacation-requests-table/vacation-requests-table.tsx
+++ src/components/screens/vacation-requests/vacation-requests-table/vacation-requests-table.tsx
@@ -1,70 +1,21 @@
 import { DataGrid } from "@mui/x-data-grid";
-import { vacationRequestStatusesAtom } from "../../../../atoms/vacationRequestStatuses";
-import { vacationRequestsAtom } from "../../../../atoms/vacationRequests";
-import { useAtom, useAtomValue, useSetAtom } from "jotai";
-import { useEffect, useRef } from "react";
+import { useSetAtom } from "jotai";
+import { useRef } from "react";
 import { Box } from "@mui/material";
-import { columns } from "./table-columns";
+import { columns } from "./vacation-requests-table-columns";
 import { selectedRowIdsAtom } from "../../../../atoms/selectedRowIds";
-import { rowsAtom } from "../../../../atoms/rows";
 import VacationRequestsSkeletonTable from "./skeleton-table/skeleton-table";
-import TableToolbar from "./table-toolbar";
-import { DataGridRow } from "../../../../types";
-import { DateTime } from "luxon";
-import { languageAtom } from "../../../../atoms/languageAtom";
+import TableToolbar from "./vacation-requests-table-toolbar";
+import VacationRequestsTableRows from "./vacation-requests-table-rows";
 
 /**
  * Vacation requests table root component, display a table of vacation requests
  *
  */
 const VacationRequestsTable = () => {
-  const vacationRequests = useAtomValue(vacationRequestsAtom);
-  const vacationRequestStatuses = useAtomValue(vacationRequestStatusesAtom);
-  const [rows, setRows] = useAtom(rowsAtom);
   const setSelectedRowIds = useSetAtom(selectedRowIdsAtom);
   const containerRef = useRef(null);
-  const language = useAtomValue(languageAtom);
-
-  useEffect(() => {
-    createRows();
-  }, [vacationRequests, vacationRequestStatuses]);
-
-  /**
-   * Create vacation requests table rows
-   */
-  const createRows = () => {
-    if (vacationRequests && vacationRequestStatuses) {
-      const tempRows: DataGridRow[] = [];
-      vacationRequests.forEach((vacationRequest) => {
-        const row: DataGridRow = {
-          id: vacationRequest.id,
-          type: vacationRequest.type,
-          updatedAt: DateTime.fromJSDate(vacationRequest.updatedAt)
-            .setLocale(language)
-            .toLocaleString(),
-          startDate: DateTime.fromJSDate(vacationRequest.startDate)
-            .setLocale(language)
-            .toLocaleString(),
-          endDate: DateTime.fromJSDate(vacationRequest.endDate)
-            .setLocale(language)
-            .toLocaleString(),
-          days: vacationRequest.days,
-          message: "No message",
-          status: "No Status"
-        };
-        vacationRequestStatuses.forEach((vacationRequestStatus) => {
-          if (vacationRequest.id === vacationRequestStatus.vacationRequestId) {
-            row.status = vacationRequestStatus.status;
-          }
-          if (vacationRequest.message.length) {
-            row.message = vacationRequest.message;
-          }
-        });
-        tempRows.push(row);
-      });
-      setRows(tempRows);
-    }
-  };
+  const rows = VacationRequestsTableRows();
 
   return (
     <Box
