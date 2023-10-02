3749e726a2bfb68c36ecdb5133ef8bef9c7bb504
Joonas Rissanen <joonas.rissanen@metatavu.fi>
2023-10-02 14:58:00 +0300

    rename header -> layout

---
+++ src/components/layout/layout.tsx
@@ -0,0 +1,24 @@
+import { Container } from "@mui/material";
+import { Outlet } from "react-router";
+import NavBar from "./navbar";
+
+/**
+ * Layout component
+ */
+const Layout = () => {
+  return (
+    <>
+      {/* HEADER START */}
+      <Container>
+        <NavBar/>
+      </Container>
+      {/* HEADER END 
+          CONTENT START */}
+      <Container sx={{ marginTop: 4 }}>
+        <Outlet />
+      </Container>
+    </>
+  );
+};
+
+export default Layout;
