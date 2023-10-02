c8f54e3f3430fcddd14ffbe7e724e88a66aa1027
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2023-10-02 15:38:28 +0300

    implement confirmation handler

---
+++ src/components/contexts/confirmation-handler.tsx
@@ -0,0 +1,80 @@
+import { DialogContent, Divider, Typography } from "@mui/material";
+import { Dispatch, ReactNode, useMemo } from "react";
+import GenericDialog from "../generics/generic-dialog";
+import { SetStateAction } from "jotai";
+
+/**
+ * Component properties
+ */
+interface Props {
+  children: ReactNode;
+  confirmation: string | undefined;
+  setConfirmation: Dispatch<SetStateAction<string | undefined>>;
+  deleteVacationsData: () => void;
+}
+
+/**
+ * confirmation handler component
+ *
+ * @param props component properties
+ */
+const ConfirmationHandler = ({
+  children,
+  confirmation,
+  setConfirmation,
+  deleteVacationsData
+}: Props) => {
+  // const [vacationRequests, setVacationRequests] = useState<[]>([]);
+
+  /**
+   * Handles confirmation message
+   *
+   * @param message confirmation message
+   */
+  const handleConfirmation = async (message: string) => {
+    setConfirmation(message);
+  };
+
+  /**
+   * Memoized context value
+   */
+  useMemo(
+    () => ({
+      setConfirmation: handleConfirmation
+    }),
+    [confirmation]
+  );
+
+  /**
+   * Component render
+   */
+  return (
+    <>
+      {children}
+      <GenericDialog
+        open={confirmation !== undefined}
+        error={false}
+        onClose={() => setConfirmation(undefined)}
+        onCancel={() => setConfirmation(undefined)}
+        onConfirm={() => {
+          setConfirmation(undefined);
+          deleteVacationsData();
+        }}
+        confirmButtonText={"Yes"}
+        cancelButtonText={"No"}
+        title={"Are you sure?"}
+      >
+        <DialogContent id="confirmation-dialog-description">
+          {confirmation && (
+            <Typography marginBottom={3} sx={{ fontSize: 16, fontWeight: "bold" }}>
+              {confirmation}
+            </Typography>
+          )}
+        </DialogContent>
+        <Divider />
+      </GenericDialog>
+    </>
+  );
+};
+
+export default ConfirmationHandler;
