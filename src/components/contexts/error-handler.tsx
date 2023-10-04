7c9caef0875c2eebed7977ca45ad0f4d077681ce
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2023-10-04 15:56:52 +0300

    remove import

--- src/components/contexts/error-handler.tsx
+++ src/components/contexts/error-handler.tsx
@@ -1,4 +1,4 @@
-import { DialogContent, Divider, Typography } from "@mui/material";
+import { Divider, Typography } from "@mui/material";
 import { useAtom } from "jotai";
 import { ReactNode, useMemo } from "react";
 import { errorAtom } from "../../atoms/error";
