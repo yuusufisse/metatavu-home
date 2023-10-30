cb67e81091b22c1c3332a436306e833ef558259d
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2023-10-30 16:01:44 +0200

    add person atom

---
+++ src/atoms/person.ts
@@ -0,0 +1,9 @@
+import { atom } from "jotai";
+import { DailyEntry, Person, PersonTotalTime, Timespan } from "../generated/client";
+
+export const personsAtom = atom<Person[]>([]);
+export const personTotalTimeAtom = atom<PersonTotalTime | undefined>(undefined);
+export const totalTimeAtom = atom<PersonTotalTime[]>([]);
+export const timespanAtom = atom<Timespan>(Timespan.ALL_TIME);
+export const personDailyEntryAtom = atom<DailyEntry | undefined>(undefined);
+export const dailyEntriesAtom = atom<DailyEntry[]>([]);
