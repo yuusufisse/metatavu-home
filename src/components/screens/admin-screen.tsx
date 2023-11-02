a4df4a779f9b0937f69fe7faf6d96c61d60dcf0a
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2023-11-02 16:03:34 +0200

    create admin screen

---
+++ src/components/screens/admin-screen.tsx
@@ -0,0 +1,21 @@
+import { Grid } from "@mui/material";
+import BalanceCard from "../home/balance-card";
+import VacationsCard from "../home/vacations-card";
+
+/**
+ * Home screen component
+ */
+const AdminScreen = () => {
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12} sm={6}>
+        <BalanceCard />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <VacationsCard />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default AdminScreen;
