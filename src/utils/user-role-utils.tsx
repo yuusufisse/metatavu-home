ef8d22f32fbce4f61cf4087f45cdd90c4649f6eb
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2023-11-03 14:35:01 +0200

    add adminMode util

--- src/utils/user-role-utils.tsx
+++ src/utils/user-role-utils.tsx
@@ -1,7 +1,31 @@
+import { useAtomValue } from "jotai";
 import { KeycloakTokenParsed } from "keycloak-js";
+import { authAtom } from "../atoms/auth";
+import { useLocation } from "react-router";
 
+/**
+ * User Role Utils class
+ */
 export default class UserRoleUtils {
-  public static isAdmin = (accessToken?: KeycloakTokenParsed): boolean => {
+  /**
+   * Check if the logged-in user has an admin role
+   *
+   * @param accessToken keycloak access token
+   * @returns boolean
+   */
+  public static isAdmin = (accessToken?: KeycloakTokenParsed) => {
     return !!accessToken?.realm_access && accessToken.realm_access?.roles.includes("admin");
+  };
+
+  /**
+   * Check if the logged-in user has admin role and is in admin route
+   *
+   * @returns boolean
+   */
+  public static adminMode = () => {
+    const { pathname } = useLocation();
+    const adminPathname = pathname.startsWith("/admin");
+    const authToken = useAtomValue(authAtom)?.token;
+    return UserRoleUtils.isAdmin(authToken) && adminPathname;
   };
 }
