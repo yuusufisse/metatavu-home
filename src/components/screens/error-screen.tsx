1425948b61dacc7d9cf87e15463db2bd3145d6ab
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2023-11-09 14:41:11 +0200

    optional error title and message

--- src/components/screens/error-screen.tsx
+++ src/components/screens/error-screen.tsx
@@ -2,17 +2,23 @@ import { useRouteError } from "react-router-dom";
 import strings from "../../localization/strings";
 
 /**
- * Error page for displaying error status text and message
- *
- * @returns ReactElement
+ * Component properties
  */
-const ErrorScreen = () => {
+interface Props {
+  title?: string;
+  message?: string;
+}
+
+/**
+ * Error page for displaying error status text and message
+ */
+const ErrorScreen = ({ title, message }: Props) => {
   const error: unknown = useRouteError();
 
   return (
     <div>
-      <h1>{strings.error.oops}</h1>
-      <p>{strings.error.generic}</p>
+      <h1>{title ? title : strings.error.oops}</h1>
+      <p>{message ? message : strings.error.generic}</p>
       <p>
         <i>{(error as Error)?.message || (error as { statusText?: string })?.statusText}</i>
       </p>
