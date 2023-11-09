773f91b0e8bb1315bc773131cd94ed28c44c707e
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2023-11-09 16:13:49 +0200

    render cards with full name and start date

--- src/components/screens/timebank-view-all-screen.tsx
+++ src/components/screens/timebank-view-all-screen.tsx
@@ -1,5 +1,34 @@
+import { useAtomValue } from "jotai";
+import { personsAtom } from "../../atoms/person";
+import { Card, CardContent, Grid, Typography } from "@mui/material";
+import { DateTime } from "luxon";
+import { languageAtom } from "../../atoms/language";
+
 const TimebankViewAllScreen = () => {
-  return <div>TimebankViewAllScreen</div>;
+  const persons = useAtomValue(personsAtom);
+  const language = useAtomValue(languageAtom);
+
+  return (
+    <Grid container spacing={1} textAlign={"center"}>
+      {persons.map((person) => {
+        return (
+          <Grid item xl={4}>
+            <Card>
+              <CardContent>
+                <Typography variant="body1">{`${person.firstName} ${person.lastName}`}</Typography>
+                <Typography>
+                  {person.startDate &&
+                    DateTime.fromFormat(person.startDate, "yyyy-M-d")
+                      .setLocale(language)
+                      .toLocaleString()}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
 };
 
 export default TimebankViewAllScreen;
