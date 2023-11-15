f9e79b6f4ecfedc71ffa6bb2de52824155cc7eb8
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2023-11-15 15:49:35 +0200

    update comment

--- src/components/screens/admin-screen.tsx
+++ src/components/screens/admin-screen.tsx
@@ -3,19 +3,17 @@ import BalanceCard from "../home/balance-card";
 import VacationsCard from "../home/vacations-card";
 
 /**
- * Home screen component
+ * Admin screen component
  */
-const AdminScreen = () => {
-  return (
-    <Grid container spacing={2}>
-      <Grid item xs={12} sm={6}>
-        <BalanceCard />
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <VacationsCard />
-      </Grid>
+const AdminScreen = () => (
+  <Grid container spacing={2}>
+    <Grid item xs={12} sm={6}>
+      <BalanceCard />
     </Grid>
-  );
-};
+    <Grid item xs={12} sm={6}>
+      <VacationsCard />
+    </Grid>
+  </Grid>
+);
 
 export default AdminScreen;
