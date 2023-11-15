3f5b22f8ee8dc13644f2a378642d3d74ed50fe82
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2023-11-15 15:18:40 +0200

    clean conditionals

--- src/utils/vacation-request-utils.ts
+++ src/utils/vacation-request-utils.ts
@@ -3,32 +3,25 @@ import { KeycloakProfile } from "keycloak-js";
 
 /**
  * Get vacation request person full name
+ * Match keycloak id from persons with vacationrequest id or user profile id
  *
  * @param props component properties
- * @returns
+ * @returns person full name as string
  */
 export const getVacationRequestPersonFullName = (
   vacationRequest: VacationRequest,
   persons: Person[],
-  userProfile: KeycloakProfile | undefined
+  userProfile?: KeycloakProfile | undefined
 ) => {
-  let foundPerson: Person | undefined;
   let personFullName = "";
-
-  if (vacationRequest) {
-    foundPerson = persons.find((person) => person.keycloakId === vacationRequest?.personId);
-  }
+  const foundPerson = persons.find((person) => person.keycloakId === vacationRequest?.personId);
 
   if (foundPerson) {
     personFullName = `${foundPerson.firstName} ${foundPerson.lastName}`;
-  } else {
-    vacationRequest?.personId;
-  }
-
-  if (personFullName === "" && userProfile && vacationRequest) {
-    if (userProfile.id === vacationRequest.personId) {
-      personFullName = `${userProfile.firstName} ${userProfile.lastName}`;
-    }
+  } else if (userProfile && userProfile.id === vacationRequest.personId) {
+    personFullName = `${userProfile.firstName} ${userProfile.lastName}`;
+  } else if (vacationRequest.personId) {
+    personFullName = vacationRequest.personId;
   }
 
   return personFullName;
