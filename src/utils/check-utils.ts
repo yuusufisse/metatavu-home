7e87affc7944cf1f9c5e5495f06f0f87cebc2e37
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2023-11-17 12:11:57 +0200

    add validation util over from metaforms

--- src/utils/check-utils.ts
+++ src/utils/check-utils.ts
@@ -13,3 +13,15 @@ export const hasAllPropsDefined = (object: object): boolean => {
   });
   return response;
 };
+
+/**
+ * Validates that given value is not undefined or null
+ *
+ * @param value value
+ * @returns true if value is not undefined or null
+ */
+export const validateValueIsNotUndefinedNorNull = <T extends Object>(
+  value: null | undefined | T
+): value is T => {
+  return value !== null && value !== undefined;
+};
