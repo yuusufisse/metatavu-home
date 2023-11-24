297e2f24471643e8b9c618a869da28738d3e5097
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2023-11-24 10:12:47 +0200

    add restricted content provider

---
+++ src/components/providers/restricted-content-provider.tsx
@@ -0,0 +1,57 @@
+import { ReactNode, useEffect, useState } from "react";
+import UserRoleUtils from "../../utils/user-role-utils";
+import ErrorScreen from "../screens/error-screen";
+import strings from "../../localization/strings";
+import { useNavigate } from "react-router";
+
+/**
+ * Admin route error screen component
+ */
+const AdminRouteErrorScreen = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate("/");
+    }, 4000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <ErrorScreen
+      message={strings.adminRouteAccess.notAdmin}
+      title={strings.adminRouteAccess.noAccess}
+    />
+  );
+};
+
+/**
+ * Component properties
+ */
+interface Props {
+  children: ReactNode;
+}
+
+/**
+ * Restricted content provider component
+ */
+const RestrictedContentProvider = ({ children }: Props) => {
+  const admin = UserRoleUtils.isAdmin();
+  const [restricted, setRestricted] = useState(admin ? false : true);
+
+  useEffect(() => {
+    if (admin) {
+      setRestricted(false);
+    } else {
+      setRestricted(true);
+    }
+  }, [admin]);
+
+  if (restricted) {
+    return <AdminRouteErrorScreen />;
+  }
+
+  return <>{children}</>;
+};
+
+export default RestrictedContentProvider;
