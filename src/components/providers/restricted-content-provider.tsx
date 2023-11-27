b4cb4be2c278c571d7a81caf531b3d6cdc0f1764
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2023-11-27 10:01:11 +0200

    move to adminrouteerrorscreen to its own component

--- src/components/providers/restricted-content-provider.tsx
+++ src/components/providers/restricted-content-provider.tsx
@@ -1,29 +1,6 @@
 import { ReactNode, useEffect, useState } from "react";
 import UserRoleUtils from "../../utils/user-role-utils";
-import ErrorScreen from "../screens/error-screen";
-import strings from "../../localization/strings";
-import { useNavigate } from "react-router";
-
-/**
- * Admin route error screen component
- */
-const AdminRouteErrorScreen = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      navigate("/");
-    }, 4000);
-    return () => clearTimeout(timer);
-  }, []);
-
-  return (
-    <ErrorScreen
-      message={strings.adminRouteAccess.notAdmin}
-      title={strings.adminRouteAccess.noAccess}
-    />
-  );
-};
+import AdminRouteErrorScreen from "../screens/admin-route-error-screen";
 
 /**
  * Component properties
