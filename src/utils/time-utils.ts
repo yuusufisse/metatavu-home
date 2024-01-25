d561b32e6b5ee16b4019e382e83216793d4ebb4f
yuusufisse <yuusufisse@gmail.com>
2024-01-25 15:38:41 +0200

    added the start and end date as variables

--- src/utils/time-utils.ts
+++ src/utils/time-utils.ts
@@ -48,14 +48,19 @@ export const formatTimePeriod = (timespan: string[] | undefined) => {
   switch (timespan.length) {
     case 1:
       return `${timespan[0]}`; // Year
+
     case 2:
-      return timespan[0].length > 4
-        ? `${DateTime.fromJSDate(new Date(timespan[0])).toLocaleString(
-            DateTime.DATE_SHORT
-          )} – ${DateTime.fromJSDate(new Date(timespan[1])).toLocaleString(DateTime.DATE_SHORT)}` // All time
-        : `${timespan[0]}/${timespan[1]}`; // Month
+      if (timespan[0].length > 4) {
+        const startDate = DateTime.fromJSDate(new Date(timespan[0])).toLocaleString(DateTime.DATE_SHORT);
+        const endDate = DateTime.fromJSDate(new Date(timespan[1])).toLocaleString(DateTime.DATE_SHORT);
+        return `${startDate} – ${endDate}`; // All time
+      } else {
+        return `${timespan[0]}/${timespan[1]}`; // Month
+      }
+
     case 3:
       return `${timespan[0]}/${timespan[2]}`; // Week
+
     default:
       return null;
   }
