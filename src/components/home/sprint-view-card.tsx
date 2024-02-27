61ee44fb70348df08de5d6ff7b8ac24d72c88d99
Koshkina Kseniia <ksusakoskina0@gmail.com>
2024-02-27 13:45:34 +0200

    added-changes

--- src/components/home/sprint-view-card.tsx
+++ src/components/home/sprint-view-card.tsx
@@ -7,38 +7,38 @@ import UserRoleUtils from "../../utils/user-role-utils";
  * SprintView card component
  */
 const SprintViewCard = () => {
-    const adminMode = UserRoleUtils.adminMode();
-    
-    return (
-        <Link
-          to={adminMode ? "/admin/sprintview" : "/sprintview"}
-          style={{ textDecoration: "none" }}
-        >
-          <Card
-            sx={{
-              "&:hover": {
-                background: "#efefef"
-              }
-            }}
-          >
-            {adminMode ? (
-              <CardContent>
-                <Typography variant="h6" fontWeight={"bold"} style={{ marginTop: 6, marginBottom: 3 }}>
-                  {strings.sprint.sprintview}
-                </Typography>
-                <Typography variant="body1">{strings.placeHolder.notYetImplemented}</Typography>
-              </CardContent>
-            ) : (
-                <CardContent>
-                  <Typography variant="h6" fontWeight={"bold"} style={{ marginTop: 6, marginBottom: 3 }}>
-                    {strings.sprint.sprintview}
-                  </Typography>
-                  <Typography variant="body1">{strings.placeHolder.notYetImplemented}</Typography>
-                </CardContent>
-            )}
-          </Card>
-        </Link>
-      );
+  const adminMode = UserRoleUtils.adminMode();
+  
+  return (
+    <Link
+      to={adminMode ? "/admin/sprintview" : "/sprintview"}
+      style={{ textDecoration: "none" }}
+    >
+      <Card
+        sx={{
+          "&:hover": {
+            background: "#efefef"
+          }
+        }}
+      >
+        {adminMode ? (
+          <CardContent>
+            <Typography variant="h6" fontWeight={"bold"} style={{ marginTop: 6, marginBottom: 3 }}>
+              {strings.sprint.sprintview}
+            </Typography>
+            <Typography variant="body1">{strings.placeHolder.notYetImplemented}</Typography>
+          </CardContent>
+        ) : (
+          <CardContent>
+            <Typography variant="h6" fontWeight={"bold"} style={{ marginTop: 6, marginBottom: 3 }}>
+              {strings.sprint.sprintview}
+            </Typography>
+            <Typography variant="body1">{strings.placeHolder.notYetImplemented}</Typography>
+          </CardContent>
+        )}
+      </Card>
+    </Link>
+  );
 }
 
 export default SprintViewCard;
