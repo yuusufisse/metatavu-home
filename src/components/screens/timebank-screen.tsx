784fd62062fb6202835ac6af532bac83af52f06b
Farah Yusuf Isse <44214375+yuusufisse@users.noreply.github.com>
2024-02-28 17:39:52 +0200

    Update src/components/screens/timebank-screen.tsx
    Co-authored-by: Antti Leppä <antti.leppa@metatavu.fi>

--- src/components/screens/timebank-screen.tsx
+++ src/components/screens/timebank-screen.tsx
@@ -46,7 +46,7 @@ const TimebankScreen = () => {
     if (loggedInPerson) {
       setSelectedEmployeeId(loggedInPerson.id);
     }
-    }, [loggedInPerson]);
+  }, [loggedInPerson]);
 
   useEffect(() => {
     getPersonDailyEntries();
