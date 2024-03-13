6785bd56210f3d8083e7e347ad4972be11c77313
cpeny003-xamk <cpeny003@edu.xamk.fi>
2024-03-13 19:46:25 +0200

    Calendar components initialized

--- src/atoms/oncall.ts
+++ src/atoms/oncall.ts
@@ -1,3 +1,5 @@
 import { atom } from "jotai";
+import { OnCallEntry } from "../types";
+import { OnCall } from "../generated/client";
 
-export const oncallAtom = atom<any[]>([]);
+export const oncallAtom = atom<OnCall[]>([]);
--- src/components/screens/on-call-calendar-screen.tsx
+++ src/components/screens/on-call-calendar-screen.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { DateCalendar, PickersDay, PickersDayProps } from "@mui/x-date-pickers";
 import { Badge } from "@mui/material";
 import { DateTime } from "luxon";
+import { useApi } from "../../hooks/use-api";
+import { oncallAtom } from "../../atoms/oncall";
+import { useAtom, useSetAtom } from "jotai";
+import { errorAtom } from "../../atoms/error";
+import strings from "../../localization/strings";
 
 
 // const renderCalendarView = () => {
@@ -25,6 +30,24 @@ function ServerDay(props: PickersDayProps<DateTime> & { highlightedDays?: number
 }
 
 const OnCallCalendarScreen = () => {
+  const { onCallApi } = useApi();
+  const [onCallData, setOnCallData] = useAtom(oncallAtom)
+  const setError = useSetAtom(errorAtom);
+
+  const getOnCallData = async () => {
+      try {
+        const fetchedData = await onCallApi.listOnCallData({year : "2024"})
+        setOnCallData(fetchedData);
+      } catch (error) {
+        setError(`${strings.error.fetchFailedGeneral}, ${error}`);
+      }
+    console.log(onCallData)
+  }
+
+  useEffect(() => {
+    getOnCallData()
+  }, [])
+  
   return (
     <>
       <DateCalendar displayWeekNumber={true} slots={{ day: ServerDay }} />
