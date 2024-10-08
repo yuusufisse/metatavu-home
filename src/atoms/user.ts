6d29adc323997aa9ff28fbc1a36a57674275f91f
Ilmo Salomäki <salomaki.ilmo@gmail.com>
2024-10-08 09:15:11 +0300

    fixed import

--- src/atoms/user.ts
+++ src/atoms/user.ts
@@ -1,4 +1,4 @@
 import { atom } from "jotai"
-import type { UsersApi } from "src/generated/homeLambdasClient"
+import type { UsersApi } from "src/generated/homeLambdasClient/apis"
 
 export const usersAtom = atom<UsersApi[]>([]);
